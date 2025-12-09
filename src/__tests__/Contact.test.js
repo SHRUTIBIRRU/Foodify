170037405f33ab6f211d4faf1ecb4fa2c8bdb0d1
Shruti Birru <shrutibirru246@gmail.com>
2025-12-09 12:28:12 +0530

    Adding setup for testing

---
+++ src/__tests__/Contact.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../components/Contact";
+import "@testing-library/jest-dom";
+
+describe("Contact Page test cases", () => {
+  test("Should load Contact us component", () => {
+    render(<Contact />);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Should load button inside Contact component", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Should load input name inside COntact component", () => {
+    render(<Contact />);
+    const input = screen.getByPlaceholderText("name");
+    expect(input).toBeInTheDocument();
+  });
+
+  test("Should load 2 input boxes on the contact component ", () => {
+    render(<Contact />);
+
+    const inputs = screen.getAllByRole("textbox");
+    expect(inputs.length).toBe(2);
+  });
+});
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,22 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <>
-      <h1>Contact</h1>
-      <h2>This is Contact us page</h2>
-    </>
+    <div className=" w-4/12 mx-auto mt-20 text-center">
+      <h1 className="font-bold text-2xl ">Contact</h1>
+      <form className="mt-2 flex flex-col justify-center gap-2">
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          className="m-2 p-2 border-2 border-green-300 rounded-lg"
+        />
+        <input
+          name="message"
+          className="m-2 p-2 border-2 border-green-300 rounded-lg"
+        />
+        <button className="m-2 p-2 bg-gray-200 rounded-lg">Submit</button>
+      </form>
+    </div>
   );
 };
 
